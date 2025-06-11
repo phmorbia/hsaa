c5781c7b12afae5ab112ec7be1ccbb29f2ba3f45
phmorbia <parth.h.morbia2@gmail.com>
2025-06-11 19:14:08 +0530

    ADDED

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    var sideNav = document.getElementById("mySidenav");
+    sideNav.style.visibility = "visible";
+    sideNav.style.width = "250px";
+}
+
+function closeNav() {
+    var sideNav = document.getElementById("mySidenav");
+    sideNav.style.visibility = "hidden";
+    sideNav.style.width = "0";
+}
